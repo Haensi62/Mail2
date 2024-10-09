52cded714b68e8e9a25d18211ecdd12b6e0c7115
Hansi2000 <termikalf@gmail.com>
2024-10-09 22:50:32 +0200

    Endlich ist die Bar oben von Betriebssystem sichtbar

--- app/src/main/java/com/example/mail2/MainActivity.kt
+++ app/src/main/java/com/example/mail2/MainActivity.kt
@@ -12,19 +12,36 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.FileProvider
+
+import androidx.core.view.WindowCompat
 import com.example.mail2.ui.theme.Mail2Theme
 import java.io.File
 import java.io.IOException
@@ -39,9 +56,73 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+// enableEdgeToEdge sorgt dafür, dass der ganze Bildschirm benutzt wird,
+// alles vom Betriebssystem wird ausgeblendet
+// enableEdgeToEdge()
+
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        // return
+
+        setContent {
+           MyApp {
+                Greeting("World")
+            }
+        }
+    }
+
+
+    // Haupt-App Composable
+    @Composable
+    fun MyApp(content: @Composable () -> Unit) {
+        MaterialTheme {
+            Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.background) {
+                content() // Hier wird der übergebene Inhalt angezeigt
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewGreeting(){
+        Greeting("Fuck You")
+    }
+
+    @Composable
+    fun Greeting(name: String)
+    {
+        val view = LocalView.current
+
+        Column () {
+            Spacer(modifier = Modifier.height(28.dp))
+                Row (modifier = Modifier.padding(all = 8.dp)) {
+                    Button(onClick = { onClickBtn1(view) }) {
+                        Text(text = "Stuff")
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(onClick = { onClickBtn2(view) }) {
+                        Text(text = "Strom")
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(onClick = { onClickBtn3(view) }) {
+                        Text(text = "Wasser")
+                    }
+                }
+                Row (modifier = Modifier.padding(all = 8.dp)){
+                    Button(onClick = { onClickBtn4(view) }) {
+                        Text(text = "Heizung")
+                    }
+                }
+            }
+    }
+
+    // Preview-Funktion für die IDE-Vorschau
+    @Composable
+    fun DefaultPreview() {
+        MyApp {
+            Greeting("Preview")
+        }
     }
 
     lateinit var photoFile: File
@@ -116,24 +197,3 @@ class MainActivity : ComponentActivity() {
 
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-
-    var modifier = Modifier
-        .padding(4.dp)  // Adds padding around the text
-        .background(Color.Cyan)  // Sets the background color
-        .border(1.dp, Color.Black)  // Adds a black border around the text
-
-    Mail2Theme {
-        Greeting("Hans-Joachim Fritz", modifier)
-    }
-}
